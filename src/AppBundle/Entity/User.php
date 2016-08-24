e2419a6e90a47cefb399af7eef8b84bed7fe3f2f
developer-01 <tigranparemuzian@gmail.com>
2016-08-24 12:30:41 +0400

    add main files

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="fos_user")
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\Repository\UserRepository")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     *
+     */
+    protected $id;
+}
+
